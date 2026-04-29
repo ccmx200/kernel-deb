ecbabfb6979494d9e774b7dfff05307a753f8f73
璀璨梦星 <arl96075@163.com>
2026-04-29 14:57:17 +0800

    Update download URLs for kernel image and headers

--- ghproxy-Update-kernel.sh
+++ ghproxy-Update-kernel.sh
@@ -10,12 +10,12 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "2. 下载编译产物：image/headers"
-wget https://ghfast.top/https://github.com/GengWei1997/kernel-deb/releases/download/v6.18/linux-image-xiaomi-raphael.deb
+wget https://up-kernel.cuicanmx.cn/linux-image-xiaomi-raphael.deb
 if [ $? -ne 0 ]; then
     echo "错误：linux-image 下载失败"
     exit 1
 fi
-wget https://ghfast.top/https://github.com/GengWei1997/kernel-deb/releases/download/v6.18/linux-headers-xiaomi-raphael.deb
+wget https://up-kernel.cuicanmx.cn/linux-headers-xiaomi-raphael.deb
 if [ $? -ne 0 ]; then
     echo "错误：linux-headers 下载失败"
     exit 1
